aeb52b59524b2ed035be01b98bcf251fd6d44b28
psuser <ci@prime-strategy.co.jp>
2019-02-23 23:47:12 +0900

    Add meta information.

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
+/*
+Plugin Name: WP Simple Framework
+Description: WordPress Development Framework. You can use this framework the way you want.
+Version: 0.0.1
+Author: Yuya Tajima
+Author URI: https://github.com/yuya-tajima/wp-framework
+License: GPLv2 or later
+*/
 
 require_once __DIR__ . '/frameworks/autoload.php';
